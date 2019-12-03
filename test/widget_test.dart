dcb8b4cdca6887ff3bc4e53a37bd2cbdc2f87a88
SKTDK <shopping@suyeon.org>
2019-12-03 14:50:48 +0100

    clear tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pfefront/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('ESOPE BD'), findsNWidgets);
-  });
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {});
 }
